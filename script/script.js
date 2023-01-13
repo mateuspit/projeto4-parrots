5281be4326b50c726ce0a94e181fe312690bd72d
Barcelos <mateuspit@hotmail.com>
2023-01-13 17:28:17 -0300

    Distribuição de cartas completo; Falta: virar corretamente, comparar

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
-alert("oi!");
-
-let gameTitle = "PARROT CARD GAME";
+const gameTitle = "PARROT CARD GAME";
 let cardNumbers = 0;
+const possibleCards = [1,1,2,2,3,3,4,4,5,5,6,6,7,7];
+const cards = [];
 
 function titleGeneration(){
     const title = document.querySelector("h1");
@@ -24,18 +24,45 @@ function getCardNumbers(){
         // console.log(higherThan14);
         
 
-    } while ((cardNumbers < 4) || (cardNumbers > 14));
+    } while ((cardNumbers < 4) || (cardNumbers > 14) || cardNumbers%2);
 }
 
 getCardNumbers();
 
-// console.log(cardNumbers);
 
-// function addCardsRandomly(){
-//     for(let i = 0; i < cardNumbers; i++){
+function comparador() { 
+	return Math.random() - 0.5; 
+}
 
-//     }
-// }
+
+
+function addCardsRandomly(){
+
+    for(let i = 0; i < cardNumbers; i++){
+        cards[i] = possibleCards[i];
+    }
+
+    cards.sort(comparador);
+
+    for(let i = 0; i < cardNumbers; i++){
+        const tableContent = document.querySelector(".cardTable");
+
+        tableContent.innerHTML = tableContent.innerHTML + `
+            <div class="card">
+                <div class="front-face face">
+                    <img src="/img/back.png" alt="Imagem do fundo da carta" width="100px">
+                </div> <!-- FRONT FACE -->
+                <div class="back-face face">
+                    <img src="/img/${cards[i]}.gif" alt="Gif numero 1" width="100px"/>
+                </div> <!-- BACK FACE -->
+            </div>
+        ` ;
+    }    
+}
+
+addCardsRandomly();
+
+
 
 
 
