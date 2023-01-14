c1302435be531d298267ff5d273ffeb11cbc4027
Barcelos <mateuspit@hotmail.com>
2023-01-14 18:10:55 -0300

    Cartas virando e respeitando as regras

--- script/script.js
+++ script/script.js
@@ -3,6 +3,12 @@ let cardNumbers = 0;
 const possibleCards = [1,1,2,2,3,3,4,4,5,5,6,6,7,7];
 const cards = [];
 let rounds = 0;
+let desvirar = 0;
+let imgList;
+let firstCardSrc = [];
+let secondCardSrc = [];
+let firstThis = [];
+let secondThis = [];
 
 function titleGeneration(){
     const title = document.querySelector("h1");
@@ -64,21 +70,53 @@ function addCardsRandomly(){
 // addCardsRandomly();
 
 function turnOnCard(showThis){
+    desvirar++;
+
+    //desvirando as cartas
     const turnedFrontCard = showThis.querySelector(".front-face");
     const turnedBackCard = showThis.querySelector(".back-face");
-    // if(classList.contains("hide-bird") === 0 ){
-    //     rounds++;        
-    // }
-    // console.log(turnedFrontCard.classList.contains("hide-bird"));
+
     if (!turnedFrontCard.classList.contains("hide-bird")){
         rounds++;
     }
+
     turnedFrontCard.classList.add("hide-bird");
     turnedBackCard.classList.add("show-gif");
-    console.log(rounds);
-    // console.log(turnedFrontCard.classList.contains("hide-bird"));
+
+    //analisando as cartas
+    if(desvirar === 1){
+        imgList = showThis.getElementsByTagName("img");
+        firstCardSrc = imgList[1].getAttribute("src");
+        firstThis[0] = turnedFrontCard;
+        firstThis[1] = turnedBackCard;
+        // console.log(firstCardSrc);
+    } else if (desvirar === 2){
+        desvirar = 0;
+        imgList = showThis.getElementsByTagName("img");
+        secondCardSrc = imgList[1].getAttribute("src");
+        secondThis[0] = turnedFrontCard;
+        secondThis[1] = turnedBackCard;
+        // console.log(secondCardSrc);
+        if(secondCardSrc === firstCardSrc){
+            console.log("Nada acontece");
+            // console.log(firstThis[0]);
+            // console.log(firstThis[1]);
+            // console.log(secondThis[0]);
+            // console.log(secondThis[1]);
+        } else {
+            // debugger;
+            setTimeout(delayOneSegHideCards, 500); 
+            console.log("Vira as cartas");
+        }        
+    }
 }
 
+function delayOneSegHideCards(){
+    firstThis[0].classList.remove("hide-bird");
+    firstThis[1].classList.remove("show-gif");
+    secondThis[0].classList.remove("hide-bird");
+    secondThis[1].classList.remove("show-gif");
+}
 
 
 
